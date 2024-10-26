cefbfa4beb623af4d7fac2292ea638da5e8d9beb
Georgyuy <56872430+Georgyuy@users.noreply.github.com>
2024-10-26 21:28:23 +0300

    Add files via upload

---
+++ Лабораторная 2/task1.py
@@ -0,0 +1,17 @@
+money_capital = 20000
+salary = 5000
+spend = 6000
+increase = 0.05
+
+months = 0
+
+while True:
+    budget = salary + money_capital
+    if budget >= spend:
+        months += 1
+        money_capital = budget - spend
+        spend *= (1 + increase)
+    else:
+        break
+
+print("Количество месяцев, которое можно протянуть без долгов:", months)
